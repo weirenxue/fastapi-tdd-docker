f1a60eddab96c6c8e68ba03b74f77aca08f77e06
Wei-Ren Xue <weirenxue@gmail.com>
2022-01-23 21:53:02 +0800

    tdd part1 finished

--- project/app/main.py
+++ project/app/main.py
@@ -1,10 +1,9 @@
-import os
 import logging
 
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger('uvicorn')
@@ -13,6 +12,7 @@ def create_application() -> FastAPI:
     application = FastAPI()
 
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
